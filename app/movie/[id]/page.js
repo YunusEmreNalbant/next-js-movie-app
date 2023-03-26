778debfbee1d90b3005483724f0949d368f62a93
Yunus Emre Nalbant <yunus.nalbant0@gmail.com>
2023-03-26 14:43:48 +0300

    mock data ile yapilmis hali

---
+++ app/movie/[id]/page.js
@@ -0,0 +1,19 @@
+import Movies from "@/mocks/movies.json";
+import {MovieContainer} from "@/containers/movie";
+import {notFound} from "next/navigation";
+
+function MoviePage({params, searchParams}) {
+    const movieDetail = Movies.results.find(movie => movie.id.toString() === params.id);
+
+    if (!movieDetail) {
+        notFound();
+    }
+
+    if (searchParams.error === 'true') {
+        throw new Error("Error happened");
+    }
+
+    return (<MovieContainer movie={movieDetail}/>)
+}
+
+export default MoviePage;
